0b6dea59caaedeca518f0f252963c3329e52b7b0
Henrique <henriquedossantos313@gmail.com>
2025-12-08 08:46:03 -0300

    Feign update

--- src/main/java/com/example/crud/infrastructure/http/config/FeignConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.infrastructure.http.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients(basePackages = "com.example.crud.client")
-public class FeignConfig {
-}
